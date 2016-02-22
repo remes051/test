7185ec292a058834cad8fcb20bd2b2b6c4285f97
remes051 <remes051@mail.ru>
2016-02-22 23:29:38 +0200

    Update UserCounter.java

--- TestProject/src/com/test/UserCounter.java
+++ TestProject/src/com/test/UserCounter.java
@@ -4,24 +4,51 @@ import java.util.Scanner;
 
 public class UserCounter {
 
-    private String LastName;
+    private String FirstName;
     private int age;
     private Scanner in;
     private UserCounter st[];
     private int i;
+    private String name;
+    private String a;
 
-    public void count(){
+    public void begin() {
+        in = new Scanner(System.in);
+        System.out.println("Hello, introduce yourself:");
+        name = in.nextLine();
+        introduce(name);
+    }
 
+    public void introduce(String name) {
+        System.out.println("Hi " + name + ", Enter 1 create new user, enter 2 to search user, 3 to exit:");
+
+    }
+
+    public void logic() {
+        a = in.nextLine();
+        if (a.equals("1")) {
+            System.out.println("");
+        }
+        if(a.equals("3")){
+            System.out.println("Exit");
+            System.exit(0);
+        }
         in = new Scanner(System.in);
         st = new UserCounter[6];
 
-        for(i=1;i<st.length;i++)
+        for (i = 1; i < st.length; i++)
 
         {
-            System.out.println("Enter name and age of "+i+" user: ");
+            System.out.println("Enter information of " + i + " user: ");
+            System.out.println("Name:");
             st[i] = new UserCounter();
-            st[i].LastName = in.nextLine();
+            st[i].FirstName = in.nextLine();
+            System.out.println("Age:");
             st[i].age = in.nextInt();
+            if (st[i].age > 100 || st[i].age < 0) {
+                System.out.println("Age should be more than 0 and less than 100, try again");
+                in.nextInt();
+            }
             in.nextLine();
         }
 
@@ -29,11 +56,12 @@ public class UserCounter {
         System.out.println();
         System.out.println("Information about users:");
 
-        for(i=1;i<st.length;i++)
+        for (i = 1; i < st.length; i++)
 
         {
-            System.out.println("Name of "+i+" user: "+st[i].LastName+" ,age: "+st[i].age);
+            System.out.println("Name of " + i + " user: " + st[i].FirstName + ", age: " + st[i].age);
         }
 
     }
+
 }
