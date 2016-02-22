5f73fe3d3b9bc991a20abba06444a9e9cdbce955
remes051 <remes051@mail.ru>
2016-02-22 23:28:53 +0200

    Update Main.java

--- TestProject/src/com/test/Main.java
+++ TestProject/src/com/test/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main (String[] args){
 
-        new UserCounter().count();
+        UserCounter user = new UserCounter();
+        user.begin();
+        user.logic();
     }
 }
